7cae485b027f9b6daf9740d63bb686a157c506f8
syq <syq@163.com>
2022-09-18 10:50:36 +0800

    the second commit

--- src/main/java/com/syq/git/GitTest.java
+++ src/main/java/com/syq/git/GitTest.java
@@ -3,5 +3,6 @@ package com.syq.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world11111");
     }
 }

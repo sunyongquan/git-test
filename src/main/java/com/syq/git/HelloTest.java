664ec0c1d6d0253245bf2c7da4c2e8ad449b212b
sunyongquan <1007584967@qq.com>
2023-04-04 21:31:56 +0800

    hot-fix first commit

--- src/main/java/com/syq/git/HelloTest.java
+++ src/main/java/com/syq/git/HelloTest.java
@@ -6,5 +6,6 @@ public class HelloTest {
         System.out.println("hello git 1 !!!");
         System.out.println("hello git 2 !!!");
         System.out.println("hello git 3 !!!");
+        System.out.println("hello git 4 !!!");
     }
 }

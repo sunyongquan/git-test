f6d248a077f57c8afa0a2a58640608ffe8a7364c
sunyongquan <1007584967@qq.com>
2023-04-04 21:34:22 +0800

    hot-fix test

--- src/main/java/com/syq/git/HelloTest.java
+++ src/main/java/com/syq/git/HelloTest.java
@@ -7,5 +7,6 @@ public class HelloTest {
         System.out.println("hello git 2 !!!");
         System.out.println("hello git 3 !!!");
         System.out.println("hello git 4 !!!");
+        System.out.println("hot-fix test !!!");
     }
 }
